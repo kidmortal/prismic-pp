07f422f1b547f1eabcf0bd4063e3c1178fe681a4
kidmortal <kidmortal@gmail.com>
2022-02-22 12:59:17 -0300

    fix param

--- .slicemachine/assets/slices/SearchFrame/index.stories.js
+++ .slicemachine/assets/slices/SearchFrame/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"search_frame","items":[],"primary":{"passagemTitle":"envisioneer efficient systems","seguroTitle":"maximize cross-platform deliverables","hotelTitle":"revolutionize granular paradigms","defaultSearch":"PASSAGEM"},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"search_frame","items":[],"primary":{"passagemTitle":"reinvent best-of-breed portals","seguroTitle":"grow 24/365 solutions","elquartoTitle":"transform end-to-end platforms","defaultSearch":"ELQUARTO"},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
